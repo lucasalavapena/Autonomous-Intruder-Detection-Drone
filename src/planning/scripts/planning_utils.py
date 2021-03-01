e72b1276d6f0361184263318ede852c3b98a46b1
Lucas Alava Peña <lucas.alavapena@gmail.com>
2021-03-01 14:44:02 +0100

    RRT working but need to test on a more complicated map

--- src/planning/scripts/planning_utils.py
+++ src/planning/scripts/planning_utils.py
@@ -9,74 +9,45 @@ def RRT(curr_x, curr_y, goal_x, goal_y, Map):
     goal_node = Node(goal_x, goal_y)
     Tree = [start_node]
 
-    while not path_found:
+    while True:
         rand_node = random_node(Map.airspace, goal_node)
+
         # closest node in tree
         nearest_node = find_nearest_node(Tree, rand_node)
-        Tree = grow(rand_node, nearest_node, Map)
+        new_node = grow(rand_node, nearest_node, Map)
+
+        if new_node != -1:
+            Tree.append(new_node)
 
         if distance(Tree[-1], goal_node) == 0:
             break
-
-    if limit:
-        times = np.arange(0, 20, 0.01)
-        controls = np.zeros(len(times) - 1)
-    else:
-        controls = generate_controls(Tree[-1])
+    controls = generate_controls(Tree[-1])
 
     return controls
 
 def generate_controls(node):
-    controls = []
-    parent = node.parent
+    controls = [(node.x, node.y)]
     tmp = node
+    parent = tmp.parent
 
     while parent is not None:
-        phis = tmp.path_phi
-        controls = phis + controls
-        tmp = tmp.parent
+        controls.append((parent.x, parent.y))
+        tmp = parent
         parent = tmp.parent
 
     return controls
 
-def grow(rand_node, nearest_node, Map):
-    coords = get_path(rand_node, nearest_node)
+def grow(rand_node, nearest_node, Map, steps = 100):
 
-    for (x,y) in coords:
-        Map.is_passable(x, y)
+    delta_x = (rand_node.x - nearest_node.x) / steps
+    delta_y = (rand_node.y - nearest_node.y) / steps
 
-
-def grow(from_node, to_node, car, obs, x_lims, y_lims):
-    steps = 100
-    tmp = from_node
-    phi = calc_phi(from_node, to_node)
-
-    x_path = [tmp.x]
-    y_path = [tmp.y]
-    phis = [phi]
-
-    for i in range(steps):
-        xn, yn, thetan = step(car, tmp.x, tmp.y, tmp.theta, phi, dt=0.01)
-        tmp = Node(xn, yn, thetan)
-
-        phi = calc_phi(tmp, to_node)
-
-        if distance(tmp, to_node) < 1.0:  # close enough to stop
-            break
-
-        x_path.append(xn)
-        y_path.append(yn)
-        phis.append(phi)
-
-    tmp.path_x = x_path[:-1]
-    tmp.path_y = y_path[:-1]
-    tmp.path_phi = phis[:-1]
-    tmp.parent = from_node
-
-    for x, y in zip(tmp.path_x, tmp.path_y):
-        if not valid(Node(x, y, 0), obs, x_lims, y_lims):
+    for i in range(1, steps):
+        x = nearest_node.x + i * delta_x
+        y = nearest_node.y + i * delta_y
+        if not Map.is_passable(x, y):
             return -1
-    return tmp
+    return rand_node
 
 def distance(from_node, to_node):
     return math.hypot(from_node.x - to_node.x, from_node.y - to_node.y)
@@ -97,7 +68,7 @@ def find_nearest_node(tree, new_node):
 def random_node(airspace, goal, prob_goal=0.2):
     x_start, y_start, _, x_end, y_end, _ = airspace
     if random.uniform(0, 1) < prob_goal:
-        return goal.copy()
+        return goal
     else:
         return Node(random.uniform(x_start, x_end), random.uniform(y_start, y_end))
 
@@ -115,10 +86,10 @@ class Node:
 # The class representing the room and its limits
 class Map:
     def __init__(self, map_path):
-
-        self.map = self.generate_map(map_path)
         self.obstacles = []
         self.airspace = None
+        self.map = self.build_map(map_path)
+
 
     def build_map(self, map_path):
         """
@@ -133,10 +104,15 @@ class Map:
         x_end, y_end, z_end = map_data["airspace"]["max"]
         self.airspace = ((x_start, y_start, z_start, x_end, y_end, z_end))
 
-        for obs in map_data["walls"]:
-            x_start, y_start, z_start = obs["plane"]["start"]
-            x_end, y_end, z_end = obs["plane"]["stop"]
-            self.obstacles.append((x_start, y_start, z_start, x_end, y_end, z_end))
+        # check if obstacles exist
+        if "walls" in map_data.keys():
+            for obs in map_data["walls"]:
+                x_start, y_start, z_start = obs["plane"]["start"]
+                x_end, y_end, z_end = obs["plane"]["stop"]
+                self.obstacles.append((x_start, y_start, z_start, x_end, y_end, z_end))
+        else:
+            print("No obstacles")
+
 
 
     def is_passable(self, x, y):
@@ -154,6 +130,10 @@ class Map:
         return True
 
 
-
+def test():
+    # world_map = Map("src/course_packages/dd2419_resources/worlds_json/planing-test.json")
+    world_map = Map("src/course_packages/dd2419_resources/worlds_json/tutorial_1.world.json")
+    result = RRT(0, 0, 1, 1.9, world_map)
+    print(result)
 if __name__ == "__main__":
-    pass
+    test()
