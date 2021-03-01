pushing detection  node. Hardcoded as of now but classifies, draws bounding box and draws axis over detected signs (correct axis though?)

src/perception/scripts/detection.py
@@ -8,7 +8,9 @@ import rospy
 import os.path
 import cv2
 import argparse
-from sensor_msgs.msg import Image
+import numpy as np
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import PoseStamped
 from cv_bridge import CvBridge, CvBridgeError
 
 import torch
@@ -36,6 +38,8 @@ class image_converter:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(
             "/cf1/camera/image_raw", Image, self.callback, queue_size=1, buff_size=2**28)
+        self.camera_param_sub = rospy.Subscriber("/cf1/camera/camera_info", CameraInfo, self.cam_callback)
+        self.camera_params = None
 
         # self.detector = Detector().to("cpu")
         self.__detector__ = Detector()
@@ -54,6 +58,26 @@ class image_converter:
         self.trans = transforms.ToTensor()
 
         # self.model = utils.load_model(self.model, MODEL, torch.device('cpu'))
+
+  def cam_callback(self, data):
+      self.camera_params = data
+
+  def draw_axis(self, img, R, t, K):
+    # unit is mm
+    rotV, _ = cv2.Rodrigues(R)
+    points = np.float32([[55, 0, 0], [0, 55, 0], [0, 0, 55], [0, 0, 0]]).reshape(-1, 3)
+    axisPoints, _ = cv2.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
+    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (255,0,0), 3)
+    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[1].ravel()), (0,255,0), 3)
+    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[2].ravel()), (0,0,255), 3)
+    return img
+
+  def draw(self, img, center, imgpts):
+    pts = imgpts.astype("int32")
+    img = cv2.line(img, center, tuple(pts[0].ravel()), (255,0,0), 5)
+    img = cv2.line(img, center, tuple(pts[1].ravel()), (0,255,0), 5)
+    img = cv2.line(img, center, tuple(pts[2].ravel()), (0,0,255), 5)
+    return img
 
   def callback(self, data):
     # Convert the image from OpenCV to ROS format
@@ -79,13 +103,16 @@ class image_converter:
         bb = self.detector.decode_output(out, 0.5)
 
         if bb:
+            height = bb[0][0]["height"].item()*-1
+            width = bb[0][0]["width"].item() # shitfix
             # bb[0][0]["width"] = torch.tensor(bb[0][0]["width"].item()*-1)
-            bb[0][0]["height"] = torch.tensor(bb[0][0]["height"].item()*-1) # shitfix
+            # bb[0][0]["height"] = torch.tensor(bb[0][0]["height"].item()*-1) # shitfix
             top_left = (int(round(bb[0][0]['x'].item())), round(bb[0][0]['y'].item()))
-            top_right = (top_left[0] + round(bb[0][0]['width'].item()), top_left[1])
-            bottom_left = (top_left[0], top_left[1] - round(bb[0][0]['height'].item()))
-            bottom_right = (top_left[0] + round(bb[0][0]['width'].item()),
-                            top_left[1] - round(bb[0][0]['height'].item()))
+            top_right = (top_left[0] + round(width), top_left[1])
+            bottom_left = (top_left[0], top_left[1] - round(height))
+            bottom_right = (top_left[0] + round(width),
+                            top_left[1] - round(height))
+            center = (int(round(top_left[0]+width/2)), int(round(top_left[1]-height/2)))
             category = utils.get_category_dict("src/perception/scripts/dd2419_detector_baseline_OG/dd2419_coco/annotations/training.json")[bb[0][0]['category']]['name']
 
             top_left = tuple((int(i) if i > 0 else 0 for i in top_left))
@@ -96,23 +123,48 @@ class image_converter:
             print("\n\n{TL}----------{TR}\n{BL}----------{BR}\n\n".format(TL=top_left, TR=top_right, BL=bottom_left, BR=bottom_right))
 
             cv2.line(cv_image, top_left, top_right, (0, 0, 255), 2) # red
-            cv2.line(cv_image, bottom_left, top_left, (0, 255, 0), 2) # green
-            cv2.line(cv_image, bottom_right, bottom_left, (255, 0, 0), 2) # blue
-            cv2.line(cv_image, bottom_right, top_right, (0, 255, 255), 2) # yellow
+            cv2.line(cv_image, bottom_left, top_left, (0, 0, 255), 2) # green
+            cv2.line(cv_image, bottom_right, bottom_left, (0, 0, 255), 2) # blue
+            cv2.line(cv_image, bottom_right, top_right, (0, 0, 255), 2) # yellow
             cv2.putText(cv_image, text=category, org=(bottom_left[0], bottom_left[1] + 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX,  
                    fontScale=0.5, color=(0,0,0), thickness=3, lineType=cv2.LINE_AA)
             cv2.putText(cv_image, text=category, org=(bottom_left[0], bottom_left[1] + 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX,  
             fontScale=0.5, color=(255,255,255), thickness=1, lineType=cv2.LINE_AA)
 
-        # - "x": Top-left corner column
-        # - "y": Top-left corner row
-        # - "width": Width of bounding box in pixel
-        # - "height": Height of bounding box in pixel
-        # - "category": Category (not implemented yet!)
+            if self.camera_params:
+                D = np.array(self.camera_params.D)
+                K = np.array(self.camera_params.K).reshape(3,3)
+                P = np.array(self.camera_params.P).reshape(3,4)
+                R = np.array(self.camera_params.R).reshape(3,3)
 
-    # res = cv2.bitwise_and(cv_image, cv_image, mask=combined_mask)
+                # 2D points in image (later would be detected keypoints with SIFT?)
+                image_points = np.array([
+                    top_left,
+                    top_right,
+                    center,
+                    bottom_left,
+                    bottom_right
+                ], dtype="double")
 
-    # Publish the image
+                # 3D coordinates of keypoints in world frame (arbitrary as of now, not quite sure see: https://learnopencv.com/head-pose-estimation-using-opencv-and-dlib/)
+                model_points = np.array([
+                    (-width/2, height/2, 0.0), # top left
+                    (width/2, height/2, 0.0), # top right
+                    (0.0, 0.0, 0.0), # center (refrence origin?)
+                    (-width/2, -height/2, 0.0), # bottom left
+                    (width/2, -height/2, 0.0) # bottom right
+                ])
+
+                success, rvec, tvec = cv2.solvePnP(model_points, image_points, K, D)
+                axis = np.array([[45.0, 0.0, 0.0], [0.0, 45.0, 0.0], [0.0, 0.0, -45.0]]).reshape(-1,3)
+                axis_points_2D, jac = cv2.projectPoints(axis, rvec, tvec, K, D)
+
+                # Both draw() and draw_axis seem to return reasonable results. Are they correct though?
+                # Will do for now (Hardcoded for MS1)
+                # cv_image = self.draw(cv_image, center, a/xis_points_2D)
+                cv_image = self.draw_axis(cv_image, R, tvec, K)
+
+
     try:
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError as e: