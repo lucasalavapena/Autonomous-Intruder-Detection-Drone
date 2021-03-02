CHANGE Z CALCULATION, BUGFIX MS2 MAP, ADD NODE TO LAUNCH
map->odom z calculation fixed
correctly rotate markers in MS2 map
add broadcast_map_odom node to launch file

src/localisation/scripts/broadcast_map_odom.py
@@ -57,10 +57,11 @@ def broadcast_marker(m):
     t1 = t_map.transform.translation.x - np.cos(yaw)*marker.pose.position.x + np.sin(yaw)*marker.pose.position.y
     t2 = t_map.transform.translation.y - np.sin(yaw) * marker.pose.position.x - np.cos(yaw) * marker.pose.position.y
 
+    print(marker.pose.position.z)
     #  Add values to transform
     t.transform.translation.x = t1
     t.transform.translation.y = t2
-    t.transform.translation.z = marker.pose.position.z
+    t.transform.translation.z = t_map.transform.translation.z - marker.pose.position.z
     br.sendTransform(t)
 
 