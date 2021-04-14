6ceaa4e0a5e7e78f576fba66b0acf13edf687642
Lucas Alava Pena <lucas.alavapena@gmail.com>
2021-04-14 15:32:37 +0200

    Fixed bugs in feature detection V3

--- src/perception/scripts/feature_detectionV2.py
+++ src/perception/scripts/feature_detectionV2.py
@@ -8,10 +8,15 @@ from dd2419_detector_baseline_OG.utils import run_model_singleimage
 
 def image_preprocessing(query_img_path, train_img_path, crop):
     img1 = cv.imread(query_img_path)  # queryImage
-    img2 = cv.imread(train_img_path)  # trainImage
+    img2 = cv.imread(train_img_path, cv.IMREAD_UNCHANGED)   # trainImage
     img2_og = img2
+    plt.imshow(img2), plt.show()
     img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
     img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+
+    plt.imshow(img1), plt.show()
+
+    plt.imshow(img2), plt.show()
 
     if crop:
         height = crop["height"].item() * -1
@@ -21,7 +26,7 @@ def image_preprocessing(query_img_path, train_img_path, crop):
         bottom_x = int(top_x + round(width))
         bottom_y = int(top_y - round(height))
         img2 = img2[top_y: bottom_y, top_x: bottom_x]
-
+        original_sign_center = ((top_x + bottom_x)/2, (top_y + bottom_y)/2)
     else:
         top_x = 0
         top_y = 0
@@ -30,12 +35,25 @@ def image_preprocessing(query_img_path, train_img_path, crop):
 
     object_center = ((img2.shape[0])/2, (img2.shape[1])/2)
 
-    return img1, img2, ((bottom_x - top_x)/2, (bottom_y - top_y)/2), object_center, img2_og
+    return img1, img2, ((bottom_x - top_x)/2, (bottom_y - top_y)/2), object_center, original_sign_center, img2_og
 
+def get_matches(des1, des2, img1, kp1, img2, kp2, display_result=False):
+    # BFMatcher with default params
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
 
-def sift_feasture_detection(query_img_path, train_img_path, crop):
-    img1, img2, center, object_points, img2_og = image_preprocessing(query_img_path, train_img_path, crop)
-    start_time = time.time()
+    # Apply ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append([m])
+
+    if display_result:
+        img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        plt.imshow(img3), plt.show()
+    return good
+def sift_feasture_detection(query_img_path, train_img_path, crop, display_result=False):
+    img1, img2, center, object_points, original_sign_center, img2_og = image_preprocessing(query_img_path, train_img_path, crop)
 
     # Initiate SIFT detector
     sift = cv.xfeatures2d.SIFT_create()
@@ -43,41 +61,65 @@ def sift_feasture_detection(query_img_path, train_img_path, crop):
     # find the keypoints and descriptors with SIFT
     kp1, des1 = sift.detectAndCompute(img1, None)
     kp2, des2 = sift.detectAndCompute(img2, None)
-    # BFMatcher with default params
-    bf = cv.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
-
-    # Apply ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append([m])
-    img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.imshow(img3), plt.show()
-    return kp1, kp2, good, img1, img2, center, object_points, img2_og
 
 
-def orb_feature_detection(query_img_path, train_img_path, crop):
-    img1, img2, center, object_points, img2_og = image_preprocessing(query_img_path, train_img_path, crop)
+    good = get_matches(des1, des2, img1, kp1, img2, kp2, display_result=display_result)
+
+
+    return kp1, kp2, good, img1, img2, center, original_sign_center, object_points, img2_og
+
+
+def orb_feature_detection(query_img_path, train_img_path, crop, display_result=False):
+    img1, img2, center, object_points, original_sign_center, img2_og = image_preprocessing(query_img_path, train_img_path, crop)
 
     # Initiate SIFT detector
-    orb = cv.ORB_create()
+    orb = cv.ORB_create() # 500, 1.2, 8, 31, 0, 2
 
     # find the keypoints and descriptors with SIFT
     kp1, des1 = orb.detectAndCompute(img1, None)
     kp2, des2 = orb.detectAndCompute(img2, None)
-    # BFMatcher with default params
-    bf = cv.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
 
-    # Apply ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append([m])
-    img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.imshow(img3), plt.show()
-    return kp1, kp2, good, img1, img2, center, object_points, img2_og
+    good = get_matches(des1, des2, img1, kp1, img2, kp2, display_result=display_result)
+
+
+    return kp1, kp2, good, img1, img2, center, original_sign_center, object_points, img2_og
+
+def surf_feature_detection(query_img_path, train_img_path, crop, display_result=False):
+    img1, img2, center, object_points, original_sign_center, img2_og = image_preprocessing(query_img_path, train_img_path, crop)
+
+    # Initiate SIFT detector
+    surf = cv.xfeatures2d.SURF_create(100, 12, 12, False, False)
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = surf.detectAndCompute(img1, None)
+    kp2, des2 = surf.detectAndCompute(img2, None)
+
+    good = get_matches(des1, des2, img1, kp1, img2, kp2, display_result=display_result)
+
+
+    return kp1, kp2, good, img1, img2, center, original_sign_center, object_points, img2_og
+
+def BRIEF_feature_detection(query_img_path, train_img_path, crop, display_result):
+    img1, img2, center, object_points, original_sign_center, img2_og = image_preprocessing(query_img_path, train_img_path, crop)
+
+    # Initiate SIFT detector
+    star = cv.xfeatures2d.StarDetector_create()
+
+    brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
+
+    # find the keypoints with STAR
+    kp1 = star.detect(img1, None)
+    kp2 = star.detect(img2, None)
+
+    # compute the descriptors with BRIEF
+    kp1, des1 = brief.compute(img1, kp1)
+    kp2, des2 = brief.compute(img2, kp2)
+
+
+    good = get_matches(des1, des2, img1, kp1, img2, kp2, display_result=display_result)
+
+
+    return kp1, kp2, good, img1, img2, center, original_sign_center, object_points, img2_og
 
 def draw(img, corners, imgpts):
     img = cv.line(img, corners, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
@@ -87,13 +129,13 @@ def draw(img, corners, imgpts):
 
 def get_points(kp1, kp2, good, object_center):
     canonical2D_kp = np.array([kp1[item[0].queryIdx].pt for item in good])
-    image_points = np.array([kp2[item[0].trainIdx].pt for item in good])
-    object_points = np.zeros((image_points.shape[0], image_points.shape[1] + 1))
+    image_points = np.array([kp2[item[0].trainIdx].pt for item in good], dtype=np.float32)
+    object_points = np.zeros((image_points.shape[0], image_points.shape[1] + 1), dtype=np.float64)
     object_points[:, :2] = (canonical2D_kp - object_center) / 10.0
 
     return object_points, image_points
 
-    # trying to deal with the duplicates of signficant points
+    # # trying to deal with the duplicates of signficant points
     #
     # image_rows, image_idx = np.unique(image_points, axis=0, return_index=True)
     # object_rows, object_idx = np.unique(object_points, axis=0, return_index=True)
@@ -106,19 +148,24 @@ def get_points(kp1, kp2, good, object_center):
 
 def get_orientation(see_image_points=False):
     my_path = os.path.abspath(os.path.dirname(__file__))
-    query_img_path = os.path.join(my_path, "dd2419_traffic_sign_pdfs", "follow_right.jpg")
+    query_img_path = os.path.join(my_path, "dd2419_traffic_sign_pdfs", "junction.jpg")
     train_img_path = os.path.join(my_path, "dd2419_detector_baseline_OG/performance_test/test_images",
                                   "0000097.jpg")
-    train_img_path = "/home/robot/test_images/v2/follow_right/G6_00136.jpg"
+    train_img_path = "/home/robot/test_images/v2/junction/G6_00068.jpg"
 
     #os.path.join(my_path, "dd2419_traffic_sign_pdfs", "G6_00138.jpg")#
 
     model_run = run_model_singleimage(train_img_path, 0.5)[0][0]
     # model_run = None
 
-    kp1, kp2, good, img1, img2, image_center, object_center, img2_og = orb_feature_detection(query_img_path, train_img_path, model_run)
-    result_img = img2
-    plt.imshow(img2), plt.show()
+    kp1, kp2, good, img1, img2, image_center_cropped, original_sign_center, object_center, img2_og =\
+                                surf_feature_detection(query_img_path, train_img_path, model_run, False)
+
+
+    # kp1, kp2, good, img1, img2, center, original_sign_center, object_points, img2_og
+
+    result_img = img2_og
+    # plt.imshow(img2), plt.show()
     # Harded for now but will be read form camera matrix
     # I got these values from camera info
     D = np.array([0.061687, -0.049761, -0.008166, 0.004284, 0.0])
@@ -128,15 +175,12 @@ def get_orientation(see_image_points=False):
     # TODO: replace camera values with a camera
     # D = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
 
-    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, 3]]).reshape(-1, 3)
 
     camera_matrix = K
     dist_coeffs = D
-    # cv.imwrite("test.jpg", result_img)
+
     object_points, image_points = get_points(kp1, kp2, good, object_center)
-    # image_points = np.array([[103, 65], [99, 126], [131, 97], [63, 152], [89, 28]], np.float32)
-    # object_points = np.zeros((image_points.shape[0], 3), np.float32)
-    # object_points[:, :2] = (np.array([[305, 106], [304, 282], [394, 194], [198, 366], [244, 5]]) - np.array([497/2, 480/2])) / 10.0
 
     if see_image_points:
         plt.imshow(img2)
@@ -146,19 +190,19 @@ def get_orientation(see_image_points=False):
         plt.scatter(object_points[:, 0] * 10.0 + object_center[0], object_points[:, 1] * 10.0 + object_center[1])
         plt.show()
 
-    retval, rvec, tvec, _ = cv.solvePnPRansac(object_points.reshape(-1, 1, 3), image_points.reshape(-1, 1, 2), camera_matrix, dist_coeffs)
+    retval, rvec, tvec, inliers = cv.solvePnPRansac(object_points.reshape(-1, 1, 3), image_points.reshape(-1, 1, 2), camera_matrix, dist_coeffs)
     # retval, rvec, tvec, inliers = cv.solvePnPRansac(object_points, image_points, camera_matrix, dist_coeffs)
     rotation_matrix, _ = cv.Rodrigues(rvec)
 
     image_points, jacobian = cv.projectPoints(axis, rvec, tvec, camera_matrix, dist_coeffs)
 
-    result_img = draw(result_img, image_center, image_points)
+    result_img = draw(result_img, original_sign_center, image_points)
     plt.imshow(result_img), plt.show()
 
 
 
 if __name__ == "__main__":
-    get_orientation(True)
+    get_orientation(False)
 
 
 
---
+++ src/perception/scripts/feature_detectionV3.py
@@ -0,0 +1,242 @@
+import os.path
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import time
+from dd2419_detector_baseline_OG.utils import run_model_singleimage
+
+class feature_detected:
+    def __init__(self, kp1, kp2, good, canon_img, drone_img, center_in_og_img, center_in_cropped_img, drone_img_og):
+        self.key_points = {1: kp1, 2: kp2}
+        self.good_matches = good
+        self.images = {"canon_img": canon_img,
+                       "drone_img": drone_img,
+                       "drone_img_og": drone_img_og}
+        self.centers = {"center_in_og_img": center_in_og_img,
+                        "center_in_cropped_img": center_in_cropped_img} 
+
+def get_camera_values():
+    D = np.array([0.061687, -0.049761, -0.008166, 0.004284, 0.0])
+    K = np.array([231.250001, 0.0, 320.519378, 0.0, 231.065552, 240.631482, 0.0, 0.0, 1.0]).reshape(3, 3)
+    P = np.array([231.25, 0.0, 322.360322, 0.0, 0.0, 231.06, 240.631, 0.0, 0.0, 0.0, 1.0, 0.0]).reshape(3, 4)
+    R = np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]).reshape(3, 3)
+
+    return D, K, P, R
+
+def image_preprocessing(canon_img_path, drone_img_path, bounding_box=None, crop=True):
+    canon_img = cv.imread(canon_img_path)  # queryImage
+    drone_img = cv.imread(drone_img_path)   # trainImage
+    drone_img_og = drone_img
+    # plt.imshow(drone_img), plt.show()
+    canon_img = cv.cvtColor(canon_img, cv.COLOR_BGR2GRAY)
+    drone_img = cv.cvtColor(drone_img, cv.COLOR_BGR2GRAY)
+
+    # plt.imshow(canon_img), plt.show()
+    #
+    # plt.imshow(drone_img), plt.show()
+
+
+    if bounding_box is not None:
+        height = bounding_box["height"].item() * -1
+        width = bounding_box["width"].item()  # shitfix
+        top_x = int(round(bounding_box['x'].item()))
+        top_y = int(round(bounding_box['y'].item()))
+        bottom_x = int(top_x + round(width))
+        bottom_y = int(top_y - round(height))
+        if crop:
+            drone_img = drone_img[top_y: bottom_y, top_x: bottom_x]
+
+        center_in_og_img = ((top_x + bottom_x)/2, (top_y + bottom_y)/2)
+        center_in_cropped_img = ((bottom_x - top_x)/2, (bottom_y - top_y)/2)
+    else:
+        top_x = 0
+        top_y = 0
+        bottom_x = canon_img.shape[0]
+        bottom_y = canon_img.shape[1]
+
+        center_in_og_img = ((drone_img.shape[0]) / 2, (drone_img.shape[1]) / 2)
+
+        center_in_cropped_img = center_in_og_img
+
+    return canon_img, drone_img, center_in_cropped_img,\
+           center_in_og_img, drone_img_og
+
+def get_matches(des1, des2, canon_img, kp1, drone_img, kp2, display_result=False):
+    # BFMatcher with default params
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # Apply ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < 0.5 * n.distance:
+            good.append([m])
+
+    if display_result:
+        img3 = cv.drawMatchesKnn(canon_img, kp1, drone_img, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        plt.imshow(img3), plt.show()
+    return good
+
+def sift_feature_detection(canon_img_path, drone_img_path, bounding_box=None, crop=False, display_result=False):
+    canon_img, drone_img, center_in_cropped_img, center_in_og_img, drone_img_og =\
+        image_preprocessing(canon_img_path, drone_img_path, bounding_box, crop)
+
+    # Initiate SIFT detector
+    sift = cv.xfeatures2d.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(canon_img, None)
+    kp2, des2 = sift.detectAndCompute(drone_img, None)
+
+
+    good = get_matches(des1, des2, canon_img, kp1, drone_img, kp2, display_result=display_result)
+
+    return feature_detected(kp1, kp2, good, canon_img, drone_img, center_in_og_img, center_in_cropped_img,
+                            drone_img_og)
+
+
+
+def orb_feature_detection(canon_img_path, drone_img_path, bounding_box=None, crop=False, display_result=False):
+    canon_img, drone_img, center_in_cropped_img, center_in_og_img, drone_img_og =\
+        image_preprocessing(canon_img_path, drone_img_path, bounding_box, crop)
+
+    # Initiate SIFT detector
+    orb = cv.ORB_create() # 500, 1.2, 8, 31, 0, 2
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = orb.detectAndCompute(canon_img, None)
+    kp2, des2 = orb.detectAndCompute(drone_img, None)
+
+    good = get_matches(des1, des2, canon_img, kp1, drone_img, kp2, display_result=display_result)
+
+
+    return feature_detected(kp1, kp2, good, canon_img, drone_img, center_in_og_img, center_in_cropped_img,
+                            drone_img_og)
+
+def surf_feature_detection(canon_img_path, drone_img_path, bounding_box=None, crop=False, display_result=False):
+    canon_img, drone_img, center_in_cropped_img, center_in_og_img, drone_img_og =\
+        image_preprocessing(canon_img_path, drone_img_path, bounding_box, crop)
+
+    # Initiate SIFT detector
+    surf = cv.xfeatures2d.SURF_create(100, 12, 12, False, False)
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = surf.detectAndCompute(canon_img, None)
+    kp2, des2 = surf.detectAndCompute(drone_img, None)
+
+    good = get_matches(des1, des2, canon_img, kp1, drone_img, kp2, display_result=display_result)
+
+
+    return feature_detected(kp1, kp2, good, canon_img, drone_img, center_in_og_img, center_in_cropped_img,
+                            drone_img_og)
+
+def BRIEF_feature_detection(canon_img_path, drone_img_path, bounding_box=None, crop=False, display_result=False):
+    canon_img, drone_img, center_in_cropped_img, center_in_og_img, drone_img_og =\
+        image_preprocessing(canon_img_path, drone_img_path, bounding_box, crop)
+
+    # Initiate SIFT detector
+    star = cv.xfeatures2d.StarDetector_create()
+
+    brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
+
+    # find the keypoints with STAR
+    kp1 = star.detect(canon_img, None)
+    kp2 = star.detect(drone_img, None)
+
+    # compute the descriptors with BRIEF
+    kp1, des1 = brief.compute(canon_img, kp1)
+    kp2, des2 = brief.compute(drone_img, kp2)
+
+
+    good = get_matches(des1, des2, canon_img, kp1, drone_img, kp2, display_result=display_result)
+
+
+    return feature_detected(kp1, kp2, good, canon_img, drone_img, center_in_og_img, center_in_cropped_img,
+                            drone_img_og)
+
+def draw(img, corners, imgpts):
+    img = cv.line(img, corners, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    img = cv.line(img, corners, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    img = cv.line(img, corners, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+    return img
+
+def get_points(kp1, kp2, good, object_center):
+    canonical2D_kp = np.array([kp1[item[0].queryIdx].pt for item in good])
+    image_points = np.array([kp2[item[0].trainIdx].pt for item in good], dtype=np.float32)
+    object_points = np.zeros((image_points.shape[0], image_points.shape[1] + 1), dtype=np.float64)
+    object_points[:, :2] = (canonical2D_kp - object_center) / 10.0
+
+    # return object_points, image_points
+
+    # trying to deal with the duplicates of signficant points
+
+    image_rows, image_idx = np.unique(image_points, axis=0, return_index=True)
+    object_rows, object_idx = np.unique(object_points, axis=0, return_index=True)
+    idx = image_idx if len(image_idx) <= len(object_idx) else object_idx
+
+    assert image_rows.shape[0] >= 4
+
+    return object_points[idx], image_points[idx]
+
+def get_orientation(see_image_points=False):
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    canon_img_path = os.path.join(my_path, "dd2419_traffic_sign_pdfs", "G6_00138.jpg")
+    # drone_img_path = os.path.join(my_path, "dd2419_detector_baseline_OG/performance_test/test_images",
+    #                               "0000097.jpg")
+    drone_img_path = "/home/robot/test_images/v2/follow_right/G6_00118.jpg"
+    # drone_img_path = os.path.join(my_path, "dd2419_traffic_sign_pdfs", "G6_00138.jpg")
+    #os.path.join(my_path, "dd2419_traffic_sign_pdfs", "G6_00138.jpg")#
+
+    bounding_box = run_model_singleimage(drone_img_path, 0.5)[0][0]
+    crop = False
+    crop_canon = True
+
+    features_detected = sift_feature_detection(canon_img_path, drone_img_path, bounding_box, crop)
+
+    result_img = features_detected.images["drone_img"] # drone image
+
+    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+    D, K, P, R = get_camera_values()
+    camera_matrix = K
+    dist_coeffs = D
+
+    object_center = features_detected.centers["center_in_og_img"]
+
+
+    object_points, image_points = get_points(features_detected.key_points[1], features_detected.key_points[2],
+                                             features_detected.good_matches, object_center)
+    if crop:
+        object_center = features_detected.centers["center_in_cropped_img"]
+
+    if see_image_points:
+        plt.imshow(features_detected.images["drone_img"])
+        plt.scatter(image_points[:, 0], image_points[:, 1])
+        plt.show()
+        plt.imshow(features_detected.images["canon_img"])
+        plt.scatter(object_points[:, 0] * 10.0 + object_center[0], object_points[:, 1] * 10.0 + object_center[1])
+        plt.show()
+
+    retval, rvec, tvec, inliers = cv.solvePnPRansac(object_points.reshape(-1, 1, 3),
+                                                    image_points.reshape(-1, 1, 2),
+                                                    camera_matrix, dist_coeffs,
+                                                    reprojectionError=8.0)
+    if see_image_points:
+        plt.imshow(features_detected.images["drone_img"])
+        plt.scatter(image_points[inliers, 0], image_points[inliers, 1])
+        plt.show()
+
+    # retval, rvec, tvec, inliers = cv.solvePnPRansac(object_points, image_points, camera_matrix, dist_coeffs)
+    rotation_matrix, _ = cv.Rodrigues(rvec)
+
+    image_points, jacobian = cv.projectPoints(axis, rvec, tvec, camera_matrix, dist_coeffs)
+
+    result_img = draw(result_img, object_center, image_points)
+    plt.imshow(result_img), plt.show()
+
+
+
+if __name__ == "__main__":
+    get_orientation(False)
+
+
+
