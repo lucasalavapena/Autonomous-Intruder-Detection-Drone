4787a8e7bcfc3af4ebe7840cbce4512a2ef35d59
ellidiivarsson <ellidi@simnet.is>
2021-04-19 17:08:29 +0200

    minor fix to battery check

--- src/utilities/scripts/battery_check.py
+++ src/utilities/scripts/battery_check.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 import rospy
-import time
 from std_msgs.msg import Float32
 
-timer = time.time()
 LOW_BATTERY = 50 # percent
 CRITICAL_BATTERY = 30 # percent
 
@@ -12,16 +10,14 @@ LOW_BATTERY_MSG = """\n####################################\n######   BATTERY LE
 NORMAL_BATTERY_MSG = "BATTERY LEVEL GOOD, CURRENTLY AT {}%"
 
 def callback(data):
-    global timer
     battery = (data.data - 3.0) / (4.23 - 3.0) * 100
 
     if battery < CRITICAL_BATTERY:
         rospy.logfatal(CRITICAL_BATTERY_MSG.format(round(battery, 2)))
     elif battery < LOW_BATTERY:
         rospy.logwarn(LOW_BATTERY_MSG.format(round(battery, 2)))
-    elif time.time() - timer > 15:
-        timer = time.time()
-        rospy.loginfo(NORMAL_BATTERY_MSG.format(round(battery, 2)))
+    else:
+        rospy.loginfo_throttle(15, NORMAL_BATTERY_MSG.format(round(battery, 2)))
 
 
 
