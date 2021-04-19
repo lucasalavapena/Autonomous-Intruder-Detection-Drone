9d475f0f1db1af01a65871456d31218966b6d720
ellidiivarsson <ellidi@simnet.is>
2021-04-19 15:21:42 +0200

    Rewrote detection code for optimization and readability

--- src/perception/scripts/detection.py
+++ src/perception/scripts/detection.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 from __future__ import print_function
 
-import roslib
-import sys
-import time
 import math
 import rospy
+import pickle
 import os.path
 import cv2
 import argparse
 import numpy as np
-from sensor_msgs.msg import Image, CameraInfo
-from geometry_msgs.msg import PoseStamped, PoseArray
+import matplotlib.pyplot as plt
+
+import tf
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import TransformStamped
 from cv_bridge import CvBridge, CvBridgeError
 from tf.transformations import quaternion_from_euler
 import torchvision.transforms.functional as TF
@@ -23,30 +24,48 @@ from torchvision import transforms
 from dd2419_detector_baseline_OG import utils
 from dd2419_detector_baseline_OG.detector import Detector
 
-
+DETECTOR = "SURF"
 LABELS_PATH = "src/perception/scripts/dd2419_detector_baseline_OG/dd2419_coco/annotations/training.json"
-my_path = os.path.abspath(os.path.dirname(__file__))
+CWD = os.path.abspath(os.path.dirname(__file__))
 MODEL_PATH = os.path.join(
-    my_path, "../models/det_2021-04-13_10-36-30-100473.pt")  # Current model
+    CWD, "../models/det_2021-04-13_10-36-30-100473.pt")  # Current model
+AXIS = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, 3]]).reshape(-1, 3)
 
 
 class image_converter:
 
     def __init__(self, device="cpu"):
+        # Publishers
         self.image_pub = rospy.Publisher("/myresult", Image, queue_size=2)
-        # self.sign_pose_pub  = rospy.Publisher('/detected_sign_pose', PoseArray, queue_size=10)
-        self.sign_pose_pub = rospy.Publisher(
-            '/detected_sign_pose', PoseStamped, queue_size=10)
+        self.br = tf.TransformBroadcaster()
 
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(
             "/cf1/camera/image_raw", Image, self.callback, queue_size=1, buff_size=2**28)
-        self.camera_param_sub = rospy.Subscriber(
-            "/cf1/camera/camera_info", CameraInfo, self.cam_callback)
-        self.camera_params = None
+
+        self.camera_params = {
+            "D": np.array([0.061687, -0.049761, -0.008166, 0.004284, 0.0]),
+            "K": np.array([231.250001, 0.0, 320.519378, 0.0, 231.065552, 240.631482, 0.0, 0.0, 1.0]).reshape(3, 3),
+            "P": np.array([231.25, 0.0, 322.360322, 0.0, 0.0, 231.06, 240.631, 0.0, 0.0, 0.0, 1.0, 0.0]).reshape(3, 4),
+            "R": np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]).reshape(3, 3)
+        }
 
         self.__detector__ = Detector()
         self.device_type = device
+
+        # Unpickling and deserializing precomputed featurepoints
+        self.features = self.unpickle()
+        self.categories = utils.get_category_dict(LABELS_PATH)
+
+        # Initializing different feature detectors
+        self.feature_detector = {
+            "SIFT": cv2.xfeatures2d.SIFT_create(),
+            "SURF": cv2.xfeatures2d.SURF_create(500),  # 500
+            "ORB": cv2.ORB_create(),
+            "STAR": cv2.xfeatures2d.StarDetector_create(),
+            "BRIEF": cv2.xfeatures2d.BriefDescriptorExtractor_create(),
+            "MATCHER": cv2.BFMatcher()
+        }
 
         if self.device_type == "cuda":
             self.device = torch.device("cuda:0")
@@ -58,43 +77,43 @@ class image_converter:
                 self.__detector__, MODEL_PATH, "cpu")
         else:
             print("Error")
-        # self.model = torch.load(MODEL, map_location=torch.device('cpu'))
-        # self.model.eval()
         self.trans = transforms.ToTensor()
 
-        # self.model = utils.load_model(self.model, MODEL, torch.device('cpu'))
 
-    def cam_callback(self, data):
-        self.camera_params = data
+    def unpickle(self):
+        # Unpickling and deserializing precomputed featurepoints
+        with open(os.path.join(CWD, 'features.pickle'), 'rb') as handle:
+            features = pickle.load(handle)
 
-    def draw_axis(self, img, R, t, K):
-        # unit is mm
-        rotV, _ = cv2.Rodrigues(R)
-        points = np.float32(
-            [[55, 0, 0], [0, 55, 0], [0, 0, 55], [0, 0, 0]]).reshape(-1, 3)
-        axisPoints, _ = cv2.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
-        img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(
-            axisPoints[0].ravel()), (255, 0, 0), 3)
-        img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(
-            axisPoints[1].ravel()), (0, 255, 0), 3)
-        img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(
-            axisPoints[2].ravel()), (0, 0, 255), 3)
-        return img
+        signs = features.keys()
+        keys = features[signs[0]].keys()
+        unpickled = {}
 
-    def draw(self, img, center, imgpts):
-        pts = imgpts.astype("int32")
-        img = cv2.line(img, center, tuple(pts[0].ravel()), (255, 0, 0), 5)
-        img = cv2.line(img, center, tuple(pts[1].ravel()), (0, 255, 0), 5)
-        img = cv2.line(img, center, tuple(pts[2].ravel()), (0, 0, 255), 5)
-        return img
+        for sign in signs:
+            unpickled[sign] = {}
+            for key in keys:
+                if key == "CENTER" or key == "IMAGE":
+                    unpickled[sign][key] = features[sign][key]
+                else:
+                    unpickled[sign][key] = {'kp': [], 'des': []}
+
+        for sign in signs:
+            for key in keys:
+                if key != "CENTER" and key != "IMAGE":
+                    kp = [cv2.KeyPoint(x=p[0][0], y=p[0][1], _size=p[1], _angle=p[2],
+                                       _response=p[3], _octave=p[4], _class_id=p[5]) for p in features[sign][key]]
+                    des = np.array([d[6] for d in features[sign][key]])
+                    unpickled[sign][key]['kp'] = kp
+                    unpickled[sign][key]['des'] = des
+
+        return unpickled
 
     def get_corners_and_cat(self, bb):
-
         x = int(round(bb['x']))
         y = int(round(bb['y']))
         height = int(round(bb["height"]*-1))  # shitfix
         width = int(round(bb["width"]))
-        category = utils.get_category_dict(LABELS_PATH)[bb['category']]['name']
+        category = self.categories[bb['category']]['name']
 
         top_left = (x, y)
         top_right = (x + width, y)
@@ -106,7 +125,6 @@ class image_converter:
         top_right = tuple(i if i > 0 else 0 for i in top_right)
         bottom_left = tuple(i if i > 0 else 0 for i in bottom_left)
         bottom_right = tuple(i if i > 0 else 0 for i in bottom_right)
-
         return top_left, top_right, bottom_left, bottom_right, center, category
 
     def draw_bb_and_cat(self, image, top_left, top_right, bottom_left, bottom_right, category):
@@ -118,6 +136,60 @@ class image_converter:
                     fontScale=0.5, color=(0, 0, 0), thickness=3, lineType=cv2.LINE_AA)
         cv2.putText(image, text=category, org=(bottom_left[0], bottom_left[1] + 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                     fontScale=0.5, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
+    # ----------------- Feature Detection -----------------
+
+    def draw(self, img, bb_center_in_drone_img, imgpts):
+        imgpts = imgpts.astype("int32")
+        img = cv2.line(img, bb_center_in_drone_img, tuple(
+            imgpts[0].ravel()), (255, 0, 0), 5)
+        img = cv2.line(img, bb_center_in_drone_img, tuple(
+            imgpts[1].ravel()), (0, 255, 0), 5)
+        img = cv2.line(img, bb_center_in_drone_img, tuple(
+            imgpts[2].ravel()), (0, 0, 255), 5)
+        return img
+
+    def detect_features(self, img, detector=DETECTOR):
+        if detector == "SURF":
+            # find the keypoints and descriptors with SIFT
+            kp, des = self.feature_detector["SURF"].detectAndCompute(img, None)
+        elif detector == "SIFT":
+            # find the keypoints and descriptors with SIFT
+            kp, des = self.feature_detector["SIFT"].detectAndCompute(img, None)
+        elif detector == "ORB":
+            # find the keypoints and descriptors with ORB
+            kp, des = self.feature_detector["ORB"].detectAndCompute(img, None)
+        elif detector == "BRIEF":
+            # find the keypoints with STAR and compute the descriptors with BRIEF
+            kp = self.feature_detector["STAR"].detect(img, None)
+            kp, des = self.feature_detector["BRIEF"].compute(img, kp)
+        return kp, des
+
+    def get_matches(self, canon_des, drone_des, canon_kp, drone_kp, canon_img, drone_img, display_result=False):
+        # BFMatcher with default params
+        matches = self.feature_detector['MATCHER'].knnMatch(
+            canon_des, drone_des, k=2)
+
+        # Apply ratio test
+        good = []
+        for m, n in matches:
+            if m.distance < 0.60 * n.distance:
+                good.append([m])
+
+        if display_result:
+            img3 = cv2.drawMatchesKnn(canon_img, canon_kp, drone_img, drone_kp,
+                                      good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            plt.imshow(img3), plt.ion(), plt.show(), plt.pause(0.001)
+        return good
+
+    def get_points(self, canon_kp, drone_kp, matches, canonical_center):
+        canonical2D_kp = np.array(
+            [canon_kp[item[0].queryIdx].pt for item in matches])
+        image_points = np.array(
+            [drone_kp[item[0].trainIdx].pt for item in matches], dtype=np.float32)
+        object_points = np.zeros(
+            (image_points.shape[0], image_points.shape[1] + 1), dtype=np.float64)
+        object_points[:, :2] = (canonical2D_kp - canonical_center) / 10.0
+        return object_points, image_points
 
     def callback(self, data):
         # Convert the image from OpenCV to ROS format
@@ -144,75 +216,72 @@ class image_converter:
             bbs = self.detector.decode_output(out, 0.7, multiple_bb=True)
 
             if bbs:
-                for bb in bbs[0]:  # TODO Check case when multiple boundin boxes detected
+                for bb in bbs[0]:
                     top_left, top_right, bottom_left, bottom_right, center, category = self.get_corners_and_cat(
                         bb)
                     self.draw_bb_and_cat(
                         cv_image, top_left, top_right, bottom_left, bottom_right, category)
 
-                    # ---------------------- Pose Estimation UNFINISHED ----------------------
-                    if self.camera_params:
-                        D = np.array(self.camera_params.D)
-                        K = np.array(self.camera_params.K).reshape(3, 3)
-                        P = np.array(self.camera_params.P).reshape(3, 4)
-                        R = np.array(self.camera_params.R).reshape(3, 3)
+                    # ----------------------- REVISED FEATURE DETECTION ----------------------
+                    # detect features
+                    kp, des = self.detect_features(cv_image)
+                    sign = category.replace(" ", "_")
+                    # find matches
+                    matches = self.get_matches(
+                        self.features[sign][DETECTOR]['des'],
+                        des,
+                        self.features[sign][DETECTOR]['kp'],
+                        kp,
+                        self.features[sign]['IMAGE'],
+                        cv_image,
+                        display_result=False
+                    )
 
-                        # 2D points in image (later would be detected keypoints with SIFT?)
-                        image_points = np.array([
-                            top_left,
-                            top_right,
-                            center,
-                            bottom_left,
-                            bottom_right
-                        ], dtype="double")
+                    if len(matches) >= 4:
+                        # get points (maybe find centerpoint first?)
+                        object_points, image_points = self.get_points(
+                            self.features[sign][DETECTOR]['kp'],
+                            kp,
+                            matches,
+                            self.features[sign]['CENTER']
+                        )
+                        # SolvePnPRansac
+                        retval, rvec, tvec, inliers = cv2.solvePnPRansac(
+                            object_points.reshape(-1, 1, 3), 
+                            image_points.reshape(-1, 1, 2), 
+                            self.camera_params["K"], 
+                            self.camera_params["D"], 
+                            useExtrinsicGuess=True)
 
-                        # 3D coordinates of keypoints in world frame (arbitrary as of now, not quite sure see: https://learnopencv.com/head-pose-estimation-using-opencv-and-dlib/)
-                        width = bb['width']
-                        height = bb['height']
-                        model_points = np.array([
-                            (-width/2, height/2, 0.0),  # top left
-                            (width/2, height/2, 0.0),  # top right
-                            (0.0, 0.0, 0.0),  # center (refrence origin?)
-                            (-width/2, -height/2, 0.0),  # bottom left
-                            (width/2, -height/2, 0.0)  # bottom right
-                        ])
+                        # project axis with result from ransac
+                        projected_axis, jacobian = cv2.projectPoints(
+                            AXIS, rvec, tvec, self.camera_params["K"], self.camera_params["D"])
 
-                        success, rvec, tvec = cv2.solvePnP(
-                            model_points, image_points, K, D)
-                        axis = np.array([[45.0, 0.0, 0.0], [0.0, 45.0, 0.0], [
-                                        0.0, 0.0, -45.0]]).reshape(-1, 3)
-                        axis_points_2D, jac = cv2.projectPoints(
-                            axis, rvec, tvec, K, D)
+                        # draw axis on drone image
+                        bb_center_in_drone_img = (
+                            bb['x'] + bb['width']/2, bb['y'] + bb['height']/2)
+                        bb_center_in_drone_img = tuple(
+                            int(i.item()) for i in bb_center_in_drone_img)
+                        cv_image = self.draw(
+                            cv_image, bb_center_in_drone_img, projected_axis)
+                        # ------------------------------------------------------------------------
 
-                        # Both draw() and draw_axis seem to return reasonable results. Are they correct though?
-                        # Will do for now (Hardcoded for MS1)
-                        # cv_image = self.draw(cv_image, center, a/xis_points_2D)
-                        cv_image = self.draw_axis(cv_image, R, tvec, K)
-                    
-                        # ---------------------------------------------------------------------------------
+                        # ---------------------- Pose Creation and publication  ----------------------
+                        t = TransformStamped()
+                        t.header.frame_id = 'cf1/camera_link'
+                        t.header.stamp = rospy.Time.now()
+                        t.child_frame_id = sign
 
+                        rotation = quaternion_from_euler(math.radians(rvec.ravel()[0]),
+                                                        math.radians(rvec.ravel()[1]),
+                                                        math.radians(rvec.ravel()[2]))
                         
-                        # ---------------------- Pose Creation and publication UNFINISHED ----------------------
-                        # Hardcoded dummy pose message as of now
-                        sign_pose = PoseStamped()
-                        sign_pose.header.frame_id = 'cf1/camera_link'
-                        sign_pose.pose.position.x = 0.0
-                        sign_pose.pose.position.y = 0.0
-                        sign_pose.pose.position.z = 0.0
-                        (sign_pose.pose.orientation.x,
-                         sign_pose.pose.orientation.y,
-                         sign_pose.pose.orientation.z,
-                         sign_pose.pose.orientation.w,) = quaternion_from_euler(math.radians(0),
-                                                                                math.radians(
-                                                                                    0),
-                                                                                math.radians(0))
-                        # sign_poses.append(sign_pose)
-                        self.sign_pose_pub.publish(sign_pose)
+                        self.br.sendTransform(
+                            tvec.ravel(), rotation, rospy.Time.now(), sign, 'cf1/camera_link') 
                         # ---------------------------------------------------------------------------------
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-            # self.sign_pose_pub.publish(sign_poses)
         except CvBridgeError as e:
             print(e)
 
