4b2dfdb1c64eddb4ab22d559ca0bf68ce7393b8d
Lucas Alava Pena <lucas.alavapena@gmail.com>
2021-04-22 11:41:29 +0200

    Undid my localisation idea and just fixed the issue with the old one (the yaw condition for the best one)

--- src/localisation/scripts/odom_publisher.py
+++ src/localisation/scripts/odom_publisher.py
@@ -111,46 +111,34 @@ def broadcast_transform(m, marker_name_extension):
 
 def data_association(m):
     """
-    Using transform detected marker->odom, calculate relative difference in yaw rotation using map->static markers of
-    [id] and send the best match to broadcast_transform() (if match).
-    :param m:
-    :return t: Transform of map->odom using the most correct marker found.
-    """
+      Using transform detected marker->odom, calculate relative difference in yaw rotation using map->static markers of
+      [id] and send the best match to broadcast_transform() (if match).
+      :param m:
+      :return t: Transform of map->odom using the most correct marker found.
+      """
     start = time.time()
     best_marker = None
     marker_name_extension = 'None found'
     best_delta = 100
     best_yaw = 100
-    str1 = str2 = str3 = ""
+
     # Find transform of pose of map in detected marker frame
     try:
-        detected = tf_buf.lookup_transform("aruco/detected" + str(m.id), 'map',  m.header.stamp, rospy.Duration(tf_timeout))
-        # detected = tf_buf.lookup_transform("map",  'cf1/base_link',  m.header.stamp, rospy.Duration(tf_timeout))
-
+        detected = tf_buf.lookup_transform("aruco/detected" + str(m.id), 'map', m.header.stamp,
+                                           rospy.Duration(tf_timeout))
     except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-        # print('odom_publisher.data_association: ', e)
+        print('odom_publisher.data_association: ', e)
         return
     trans_detected, rot_detected = transform_stamped_to_pq(detected)
     detected_mat = np.dot(translation_matrix(trans_detected), quaternion_matrix(rot_detected))
 
-    # Find transform of the position of the drone in map
-    try:
-        drone_loc = tf_buf.lookup_transform("map",  'cf1/base_link',  m.header.stamp, rospy.Duration(tf_timeout))
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-        # print('odom_publisher.data_association: ', e)
-        return
-    trans_loc, rot_loc = transform_stamped_to_pq(drone_loc)
-    loc_mat = np.dot(translation_matrix(trans_loc), quaternion_matrix(rot_loc))
-
-    # rospy.loginfo_throttle(2, "trans_loc \n{}".format(trans_loc))
     n = 0
     while True:
         # Find transform of pose of static marker in map
         try:
             t_map = tf_buf.lookup_transform('map', "aruco/marker" + str(m.id) + '_' + str(n), m.header.stamp)
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            # print(e)
-            rospy.loginfo_throttle(1, "break bc n = %d", n)
+            print(e)
             break  # if n exceeds number for last non-unique static marker in map, end the loop.
         trans_map, rot_map = transform_stamped_to_pq(t_map)
         map_mat = np.dot(translation_matrix(trans_map), quaternion_matrix(rot_map))
@@ -159,66 +147,24 @@ def data_association(m):
         result_mat = np.dot(map_mat, detected_mat)
         trans_result = translation_from_matrix(result_mat)
         rot_result = quaternion_from_matrix(result_mat)
-        # if n == 0:
-        #     rospy.loginfo_throttle(5, "n=0  result_mat \n{}".format(result_mat))
-        # elif n == 1:
-        #     rospy.loginfo_throttle(5, "n=1  result_mat \n{}".format(result_mat))
-        # elif n == 2:
-        #     rospy.loginfo_throttle(5, "n=2  result_mat \n{}".format(result_mat))
 
         d_roll, d_pitch, d_yaw = euler_from_quaternion(rot_result)
-        orientation_error = 1 * math.pi/6
-
-        distance_from_sign = trans_loc - trans_map
-
-
-        if n == 0:
-            str0 = "n=0  distance_from_sign \n{} \ntrans_loc: \n{}\n".format(distance_from_sign, trans_loc)
-            rospy.loginfo_throttle(1, "n=0  distance_from_sign \n{} \ntrans_loc: \n{}\n".format(distance_from_sign,
-                                                                                                            trans_loc))
-
-        elif n == 1:
-            rospy.loginfo_throttle(1, "n=1  distance_from_sign \n{} \ntrans_locc: \n{}\n".format(distance_from_sign,
-                                                                                                           trans_loc))
-            str1 = "n=1  distance_from_sign \n{} \ntrans_loc: \n{}\n".format(distance_from_sign, trans_loc)
-
-        elif n == 2:
-            str2 = "n=2  distance_from_sign \n{} \ntrans_loc: \n{}\n".format(distance_from_sign, trans_loc)
-            rospy.loginfo_throttle(1, "n=2  distance_from_sign \n{} \ntrans_loc: \n{}\n".format(distance_from_sign,
-                                                                                                           trans_loc))
-        result_str = str1 + str1 + str2
-        debug_pub.publish(result_str)
-        # rospy.loginfo_throttle(2, "trans_loc \n{}".format(trans_loc))
-        # d_yaw = 89999
-
+        orientation_error = math.pi / 6
         if np.abs(d_yaw) <= orientation_error:
-            delta = np.linalg.norm(distance_from_sign)
-
-            if n == 0:
-                rospy.loginfo_throttle(0.6, "trans_result {}".format(trans_loc))
-                rospy.loginfo_throttle(0.6, "delta_norm for {} is {};\n yaw info: best {} curr {}".format(str(m.id) + '_' + str(n), delta, best_yaw, d_yaw))
-            elif n == 1:
-                rospy.loginfo_throttle(0.6, "trans_result {}".format(trans_loc))
-                rospy.loginfo_throttle(0.6, "delta_norm for {} is {};\n yaw info: best {} curr {}".format(str(m.id) + '_' + str(n), delta, best_yaw, d_yaw))
-            elif n == 2:
-                rospy.loginfo_throttle(0.6, "trans_result {}".format(trans_loc))
-                rospy.loginfo_throttle(0.6, "delta_norm for {} is {};\n yaw info: best {} curr {}".format(str(m.id) + '_' + str(n), delta, best_yaw, d_yaw))
-            #
-
+            delta = np.linalg.norm(trans_result)
+            print(
+                "delta_norm for {} is {};\n yaw info: best {} curr {}".format(str(m.id) + '_' + str(n), delta, best_yaw,
+                                                                              d_yaw))
             if (best_marker is None or d_yaw <= best_yaw) and delta < best_delta:
                 best_marker = t_map
                 best_delta = delta
                 best_yaw = d_yaw
                 marker_name_extension = str(m.id) + '_' + str(n)
-                rospy.loginfo_throttle(0.6, "delta_norm for {} is {};\n yaw info: best {} curr {}".format(str(m.id) + '_' + str(n), delta, best_yaw, d_yaw))
-
-                # print(marker_name_extension)
         n += 1
-    # print('best: ' + marker_name_extension)
+    print('best: ' + marker_name_extension)
     end = time.time()
     if best_delta == 100 or best_yaw == 100:  # If no marker found was good enough, return nothing
         return None
-    rospy.loginfo_throttle(1, "Sending transform...n = %d", n)
     return broadcast_transform(m, marker_name_extension)
 
 
@@ -282,7 +228,6 @@ br = tf2_ros.TransformBroadcaster()
 sub_marker = rospy.Subscriber('/aruco/markers', MarkerArray, marker_callback)
 sub_unique = rospy.Subscriber('/marker/unique', Int16, unique_callback)
 pub = rospy.Publisher('localisation/is_localised', Bool, queue_size=10)
-debug_pub = rospy.Publisher('lucas/debug', String, queue_size=2**10)
 
 tf_timeout = rospy.get_param('~tf_timeout', 0.1)
 print('Ready')
