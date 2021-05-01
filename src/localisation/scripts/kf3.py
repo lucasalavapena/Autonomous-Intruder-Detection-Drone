8ab7e0ffc065f3b76190955f6e8886a59c6f4fbe
Lucas Alava Pena <lucas.alavapena@gmail.com>
2021-05-01 18:01:45 +0200

    kf3 as a class

--- src/localisation/scripts/kf3.py
+++ src/localisation/scripts/kf3.py
@@ -9,125 +9,135 @@ from numpy import dot, sum, tile, linalg, exp, log, pi, diag, eye, zeros, array
 from numpy.linalg import inv, det
 from numpy.random import randn
 
-first_time = True
 
-# MOTION MODEL
-# Coefficients of motion variables
-A = eye(3)
-# Control from motion model
-B = zeros([3, 3])
-u = zeros([3, 1])
-# Covariance of motion model
-R = eye(3)
+class kalman_filter:
+    def __init__(self):
 
-# MEASUREMENT MODEL
-# Coefficients of measurement variables
-C = array([1, 1, 1])
-# Covariance of measurement model
-Q = 1
+        self.first_time = True
 
-mu = None
-sigma = None
-z = None
+        # MOTION MODEL
+        # Coefficients of motion variables
+        self.A = eye(3)
+        # Control from motion model
+        self.B = zeros([3, 3])
+        self.u = zeros([3, 1])
+        # Covariance of motion model
+        self.R = eye(3)*0.1
+
+        # MEASUREMENT MODEL
+        # Coefficients of measurement variables
+        self.C = eye(3) #array([[1, 1, 1]])
+        # Covariance of measurement model
+        self.Q = 1
+
+        self.mu = None
+        self.sigma = None
+        self.z = None
 
 
-def pose_callback(msg):
-    global mu, sigma, z, first_time
+        self.pose_sub = rospy.Subscriber('/kf3/input', TransformStamped, self.pose_callback,
+                                         queue_size=1, buff_size=2 ** 24)
+        self.pub = rospy.Publisher('kf3/output', TransformStamped, queue_size=10)
 
-    p, q = transform_stamped_to_pq(msg)
-    roll, pitch, yaw = euler_from_quaternion(q)
-    # Mean of the state: [x, y, yaw]
-    mu = array([[p[0]], [p[1]], [yaw]])  
+    def pose_callback(self, msg):
+        p, q = self.transform_stamped_to_pq(msg)
+        roll, pitch, yaw = euler_from_quaternion(q)
+        # Mean of the state: [x, y, yaw]
+        self.mu = array([[p[0]], [p[1]], [yaw]])
+        # self.mu = array([[2], [3], [4]])
+        if self.first_time:
+            # Covariance of the state
+            self.sigma = eye(3)*0.01
+            # Measurment model
+            self.z = array([[self.mu[0, 0]],
+                             [self.mu[1, 0]],
+                             [self.mu[2, 0]]
+                            ])
+                    #array([self.mu[0, 0],
+                    #       self.mu[1, 0],
+                    #       self.mu[2, 0]
+                    #       ])
 
-    if first_time:
-        # Covariance of the state
-        sigma = eye(3)*0.1 
+        # Estimated belief
+        self.mu, self.sigma = self.kf_predict()
+        print('after predict')
+        print(self.sigma)
+
+        # Posterior distribution and Kalman Gain
+        self.mu, self.sigma, K = self.kf_update()
+        print('after update')
+        print(self.sigma)
+
         # Measurment model
-        z = array([[mu[0, 0], [mu[1, 0]], [mu[2, 0]])
-
-    # Estimated belief
-    mu, sigma = kf_predict(mu, sigma, A, R, B, u)
-    print('after predict')
-    print(sigma)
-
-    # Posterior distribution and Kalman Gain
-    mu, sigma, K = kf_update(mu, sigma, z, C, Q)
-    print('after update')
-    print(sigma)
-
-    # Measurment model
-    z = array([[p[0]], [p[1]], [yaw]])
-
-    # Update the transform with the updated variables
-    msg.transform.translation.x = mu[0, 0]  # x
-    msg.transform.translation.y = mu[1, 0]  # y
-
-    (msg.transform.rotation.x,
-     msg.transform.rotation.y,
-     msg.transform.rotation.z,
-     msg.transform.rotation.w) = quaternion_from_euler(0, 0, mu[2, 0])  # yaw
-
-    pub.publish(msg)
-    first_time = False
+        self.z = array([[p[0]], [p[1]], [yaw]]) #array([p[0], p[1], yaw])
+        # self.z = array([2, 3, 4]) #array([[2], [3], [4]])
 
 
-def kf_predict(mu, sigma, A, R, B, u):
-    # Estimated mean of the state
-    mu = dot(A, mu) + dot(B, u)
+        # Update the transform with the updated variables
+        msg.transform.translation.x = self.mu[0, 0]  # x
+        msg.transform.translation.y = self.mu[1, 0]  # y
 
-    # Estimated covariance of the state
-    sigma = dot(A, dot(sigma, A.T)) + R
+        (msg.transform.rotation.x,
+         msg.transform.rotation.y,
+         msg.transform.rotation.z,
+         msg.transform.rotation.w) = quaternion_from_euler(0, 0, self.mu[2, 0])  # yaw
 
-    return mu, sigma
+        self.pub.publish(msg)
+        self.first_time = False
 
+    def transform_to_pq(self, msg):
+        """Convert a C{geometry_msgs/Transform} into position/quaternion np arrays
 
-def kf_update(mu, sigma, z, C, Q):
-    # Kalman Gain
-    K = dot(sigma, dot(C.T, inv(dot(C, dot(cov, C.T)) + Q)))
+        @param msg: ROS message to be converted
+        @return:
+          - p: position as a np.array
+          - q: quaternion as a numpy array (order = [x,y,z,w])
+        """
+        p = array([msg.translation.x, msg.translation.y, msg.translation.z])
+        q = array([msg.rotation.x, msg.rotation.y, msg.rotation.z, msg.rotation.w])
+        return p, q
 
-    # Innovation
-    eta = z - dot(C, mu)
+    def transform_stamped_to_pq(self, msg):
+        """Convert a C{geometry_msgs/TransformStamped} into position/quaternion np arrays
 
-    # Compute posterior
-    mu = mu + dot(K, eta) # Mean of the state
-    I = eye(2)
-    sigma = dot((I - dot(K, C)), sigma) # Covariance of the state
-    
-    return mu, sigma, K
+        @param msg: ROS message to be converted
+        @return:
+          - p: position as a np.array
+          - q: quaternion as a numpy array (order = [x,y,z,w])
+        """
+        return self.transform_to_pq(msg.transform)
 
-def transform_to_pq(msg):
-    """Convert a C{geometry_msgs/Transform} into position/quaternion np arrays
+    def kf_predict(self):
+        # Estimated mean of the state
+        mu = dot(self.A, self.mu) + dot(self.B, self.u)
 
-    @param msg: ROS message to be converted
-    @return:
-      - p: position as a np.array
-      - q: quaternion as a numpy array (order = [x,y,z,w])
-    """
-    p = array([msg.translation.x, msg.translation.y, msg.translation.z])
-    q = array([msg.rotation.x, msg.rotation.y, msg.rotation.z, msg.rotation.w])
-    return p, q
+        # Estimated covariance of the state
+        sigma = dot(self.A, dot(self.sigma, self.A.T)) + self.R
 
-
-def transform_stamped_to_pq(msg):
-    """Convert a C{geometry_msgs/TransformStamped} into position/quaternion np arrays
-
-    @param msg: ROS message to be converted
-    @return:
-      - p: position as a np.array
-      - q: quaternion as a numpy array (order = [x,y,z,w])
-    """
-    return transform_to_pq(msg.transform)
+        return mu, sigma
 
 
-def main():
-    rospy.spin()
+    def kf_update(self):
+        # Kalman Gain
+        K_term = dot(self.C.T, 1/(dot(self.C, dot(self.sigma, self.C.T)) + self.Q))
+        K = dot(self.sigma, K_term)
+        # Innovation
+        eta = self.z - dot(self.C, self.mu)
+
+        #print(self.z.shape, K.shape, eta.shape)
+        # Compute posterior
+        mu = self.mu + dot(K, eta) # Mean of the state
+        I = eye(3)
+        sigma = dot((I - dot(K, self.C)), self.sigma) # Covariance of the state
+
+        return mu, sigma, K
 
 
 if __name__ == '__main__':
     print('Starting...')
-    rospy.init_node('kf3')
-    pose_sub = rospy.Subscriber('/kf3/input', TransformStamped, pose_callback, queue_size=1, buff_size=2 ** 24)
-    pub = rospy.Publisher('kf3/output', TransformStamped, queue_size=10)
     print('Ready')
-    main()
+
+    rospy.init_node('kf3')
+    kf = kalman_filter()
+    rospy.spin()
 
